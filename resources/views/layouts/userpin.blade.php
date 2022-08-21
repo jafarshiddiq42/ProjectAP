6f3e0eda8705effbc371af1723e65a1071db8539
jafarshiddiq42 <105173237+jafarshiddiq42@users.noreply.github.com>
2022-08-21 20:00:46 +0700

    Update userpin.blade.php

--- resources/views/layouts/userpin.blade.php
+++ resources/views/layouts/userpin.blade.php
@@ -58,14 +58,14 @@
                             </div>
                         </h6>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#!">
-                            <div class="dropdown-item-icon"><i data-feather="settings"></i></div>
-                            Account
-                        </a>
-                        <a class="dropdown-item" href="#!">
+                       
+                        <a class="dropdown-item" href="#!" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
                             <div class="dropdown-item-icon"><i data-feather="log-out"></i></div>
                             Logout
                         </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
                     </div>
                 </li>
             </ul>
