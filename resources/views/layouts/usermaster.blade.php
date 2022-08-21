9607b9386a899875063a12de8f01f3fa4d02072c
jafarshiddiq42 <105173237+jafarshiddiq42@users.noreply.github.com>
2022-08-21 20:17:14 +0700

    Update usermaster.blade.php

--- resources/views/layouts/usermaster.blade.php
+++ resources/views/layouts/usermaster.blade.php
@@ -28,7 +28,7 @@
             <!-- * * Tip * * You can use text or an image for your navbar brand.-->
             <!-- * * * * * * When using an image, we recommend the SVG format.-->
             <!-- * * * * * * Dimensions: Maximum height: 32px, maximum width: 240px-->
-         <a class="navbar-brand pe-3 ps-4 ps-lg-2" href="index.html">
+         <a class="navbar-brand pe-3 ps-4 ps-lg-2" href="/dashboard">
             Dayah Darul Ihsan
          </a>
             <!-- Navbar Search Input-->
